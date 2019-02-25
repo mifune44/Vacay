78177c18931f3e375916b0f75949a49703ede5fb
Joshua Landry <46336653+mifune44@users.noreply.github.com>
2019-02-25 14:31:23 -0800

    Cleaned up html and added basic logic form in jquery
    Added name option.  Added basic logic forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,99 @@
-$(document).ready(function(){
-	$("h1").click(function() {
-	  alert("This is a header.");
-	});
+$(document).ready(function() {
 
-	$("p").click(function() {
-	  alert("This is a paragraph.");
-	});
+  $("form#user-prefs").submit(function(event) {
+    var name = $("input#name").val();
+    $(".fillInName").text(name);
+  });
 
-	$("img").click(function() {
-	  alert("This is an image.");
+  $("form#user-prefs").submit(function(event) {
+    var name = $("input#name").val();
+    var weather = $("select#weather").val();
+    var cost = $("select#cost").val();
+    var where = $("select#where").val();
+    var vice = $("select#vice").val();
+    var oblige = $("select#oblige").val();
+    
+    if (weather === 'hot' && cost === 'rich') {
+          $("#dubai").fadeIn();
+          $("#siberia").hide();
+          $("#zurich").hide();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+        } else if (weather === 'hot' && cost === 'cheap') {
+          $("#dubai").hide();
+          $("#siberia").hide();
+          $("#zurich").hide();
+          $("#sierra_leone").fadeIn();
+          $("#hell").hide();
+        } else if (weather === 'cold' && cost === "rich") {
+          $("#dubai").hide();
+          $("#siberia").hide();
+          $("#zurich").fadeIn();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+      } else if (weather === 'cold' && cost === 'cheap') {
+          $("#dubai").hide();
+          $("#siberia").fadeIn();
+          $("#zurich").hide();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+        } else if (vice === 'cold') {
+          $("#dubai").hide();
+          $("#siberia").hide();
+          $("#zurich").hide();
+          $("#sierra_leone").fadeIn();
+          $("#hell").hide();
+        } else if (cost === '' && where === '') {
+          $("#dubai").hide();
+          $("#siberia").hide();
+          $("#zurich").fadeIn();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+        } else if (cost === '' && where === '') {
+          $("#dubai").fadeIn();
+          $("#siberia").hide();
+          $("#zurich").hide();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+        } else if (oblige === '' || cost === '' || where === '') {
+          $("#dubai").hide();
+          $("#siberia").fadeIn();
+          $("#zurich").hide();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+        } else if (vice === 'cool' && where === '') {
+          $("#dubai").hide();
+          $("#siberia").hide();
+          $("#zurich").hide();
+          $("#sierra_leone").fadeIn();
+          $("#hell").hide();
+        } else if (vice === 'warm' || oblige === '') {
+          $("#dubai").hide();
+          $("#siberia").fadeIn();
+          $("#zurich").hide();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+        } else if (vice === 'dry') {
+          $("#dubai").hide();
+          $("#siberia").hide();
+          $("#zurich").fadeIn();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+        } else if ( === 'luxe') {
+          $("#dubai").hide();
+          $("#siberia").fadeIn();
+          $("#zurich").hide();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+        } else {
+          $("#dubai").hide();
+          $("#siberia").hide();
+          $("#zurich").fadeIn();
+          $("#sierra_leone").hide();
+          $("#hell").hide();
+        }
+    console.log(weather);
+
+        event.preventDefault();
+      });
     });
-});
